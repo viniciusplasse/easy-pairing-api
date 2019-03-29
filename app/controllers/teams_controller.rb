e230a9df12cc4b5b1ce0de82580a99c1b8ae749e
hscorrea <hscorrea@thoughtworks.com>
2019-03-29 13:22:23 -0300

    Saves a team and its members on the database after a POST /teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,11 +2,21 @@ class TeamsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    name = params[:name]
-    members = params[:members]
+    team_name = params[:name]
+    team_members = params[:members]
 
-    if name.nil? || members.nil? || members.length < 3
+    if team_name.nil? || team_members.nil? || team_members.length < 3
       return head :bad_request
+    end
+
+    begin
+      team = Team.create(name: team_name)
+
+      team_members.each do |member|
+        Member.create(name: member, team_id: team.id)
+      end
+    rescue StandardError
+      return head :internal_server_error
     end
 
     head :created
--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -5,44 +5,75 @@ RSpec.describe TeamsController, type: :controller do
   describe "#create" do
     describe "returns bad request" do
       it "when name is missing" do
-        team = {
+        @team = {
           members: ['John', 'Mary', 'Joseph', 'Claudia']
         }
-        
-        post :create, params: team 
-  
-        expect(response).to have_http_status(:bad_request)
       end
-  
+
       it "when member list is missing" do
-        team = {
+        @team = {
           name: 'Example team'
         }
-  
-        post :create, params: team 
-  
-        expect(response).to have_http_status(:bad_request)
       end
-  
+
       it "when members length is less than 3" do
-        team = {
+        @team = {
           name: 'Example team',
           members: ['John', 'Mary']
         }
-  
-        post :create, params: team 
-  
+      end
+
+      after :each do
+        post :create, params: @team
+
         expect(response).to have_http_status(:bad_request)
       end
     end
 
-    it "returns created if everything is ok" do
-      team = {
+    describe "returns internal server error" do
+      before :each do
+        @team = {
+          name: 'Example Team',
+          members: ['John', 'Mary', 'Joseph', 'Claudia']
+        }
+      end
+
+      it "if there is a failure on team creation" do
+        allow(Team).to receive(:create).and_raise(StandardError)
+      end
+
+      it "if there is a failure on any member creation" do
+        allow(Member).to receive(:create).and_raise(StandardError)
+      end
+
+      after :each do
+        post :create, params: @team
+
+        expect(response).to have_http_status(:internal_server_error)
+      end
+    end
+
+    it "returns created (and saves on database) if everything is ok" do
+      request_body = {
         name: 'Example Team',
         members: ['John', 'Mary', 'Joseph', 'Claudia']
       }
 
-      post :create, params: team 
+      team_mock = Team.new
+      team_mock.id = 123
+      team_mock.name = 'Example Team'
+
+      expect(Team)
+        .to receive(:create)
+        .with(name: 'Example Team')
+        .and_return(team_mock)
+
+      expect(Member).to receive(:create).with(name: 'John', team_id: 123)
+      expect(Member).to receive(:create).with(name: 'Mary', team_id: 123)
+      expect(Member).to receive(:create).with(name: 'Joseph', team_id: 123)
+      expect(Member).to receive(:create).with(name: 'Claudia', team_id: 123)
+
+      post :create, params: request_body
 
       expect(response).to have_http_status(:created)
     end
